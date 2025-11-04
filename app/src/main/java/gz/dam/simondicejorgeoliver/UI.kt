1b68edda504a360e78cd214a458d13728e88c849
dam <jdurancruz@danielcastelao.org>
2025-11-04 11:52:35 +0100

    Puntuacion añadida

--- app/src/main/java/gz/dam/simondicejorgeoliver/UI.kt
+++ app/src/main/java/gz/dam/simondicejorgeoliver/UI.kt
@@ -1,8 +1,10 @@
 package gz.dam.simondicejorgeoliver
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,11 +13,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun UI(){
@@ -25,7 +29,22 @@ fun UI(){
 
 @Composable
 fun Menu(){
-    Botonera()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Column (modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Puntuacion(1)
+            Botonera()
+        }
+
+    }
+
+}
+
+@Composable
+fun Puntuacion(puntuacion: Int){
+    Text(
+        text = "Puntuación: $puntuacion",
+        modifier = Modifier.padding(top = 100.dp)
+    )
 }
 
 
